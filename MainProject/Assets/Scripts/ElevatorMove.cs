640c9917c44a885015967ad84f43d2c8f3d362dc
cclimited <blagmyea@live.com.au>
2021-05-10 23:51:16 +1000

    Parenting
    Player now moves more smoothly with elevator

--- MainProject/Assets/Scripts/ElevatorMove.cs
+++ MainProject/Assets/Scripts/ElevatorMove.cs
@@ -7,30 +7,45 @@ public class ElevatorMove : MonoBehaviour
     public Animator animator;
     public GameObject instructions;
 
+    public GameObject player;
+    
+
+    void Start()
+    {
+        player = Player.instance.gameObject;
+    }    
 
     void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject == player)
         {
+            player.transform.parent = transform;
+        }
 
+
+        if (other.CompareTag("Player"))
+        {            
             instructions.SetActive(true);
             if (Input.GetKeyDown(KeyCode.F))
                 animator.SetTrigger("MoveForward");
 
             if (Input.GetKeyDown(KeyCode.B))
                 animator.SetTrigger("MoveBackward");
-
-
-        }
-
-    }
+        }  
+        
+    }    
 
     void OnTriggerExit(Collider other)
     {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+
         if (other.CompareTag("Player"))
         {
             instructions.SetActive(false);
-        }
+        }       
 
     }
 }
